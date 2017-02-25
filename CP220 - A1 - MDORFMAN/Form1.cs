b57430f1b6d49fa61f3f9a20cfc613d1904ca126
Michael Dorfman <mvd1409@gmail.com>
2017-02-25 12:40:40 -0500

    Completed Classes

--- CP220 - A1 - MDORFMAN/Form1.cs
+++ CP220 - A1 - MDORFMAN/Form1.cs
@@ -17,4 +17,44 @@ namespace CP220___A1___MDORFMAN
             InitializeComponent();
         }
     }
+
+    public class starship
+    {
+        // Fields
+        private string ship_type;
+        private int shield_level = 100;
+        private int health, max_health;
+        private string[] condition = new string[] { "Destroyed", "Damaged", "Pristine" };
+
+    }
+
+    public class cruiser : starship
+    {
+
+    }
+
+    public class destroyer : starship
+    {
+
+    }
+
+    public class ordinance
+    {
+
+    }
+
+    public class torpedo : ordinance
+    {
+
+    }
+
+    public class ion_bomb: ordinance
+    {
+
+    }
+
+    public class laser : ordinance
+    {
+
+    }
 }
